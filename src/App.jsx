061588a328539f71e6507393d106044cc591f337
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-07 08:45:47 +0200

    Create and export header component while importing in the App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
+import Header from "./components/Header.jsx";
+
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+  return (
+    <header>
+      <img src="https://via.placeholder.com/150" alt="Logo" />
+      <h2>Header Component</h2>
+      <h4>React Course - Project 3</h4>
+    </header>
+  );
+}
+
