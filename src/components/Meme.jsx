9ee1362433f204fff7b492efe881020a2298d4da
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-07 10:34:13 +0200

    Create and style form

---
+++ src/components/Meme.jsx
@@ -0,0 +1,27 @@
+export default function Meme() {
+    return (
+    <main>
+        <form className="form">
+
+            <input 
+                type="text" 
+                className="form--input" 
+                placeholder="Shut Up"
+            />
+            
+            <input 
+                type="text" 
+                className="form--input" 
+                placeholder="and take my money"
+            />
+
+            <button 
+            className="form--button"
+            >
+                Generate a new image 🖼️
+            </button>
+            
+        </form>
+    </main>
+    );
+}
