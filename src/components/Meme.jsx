5ebf0513f284054145c349432859de7463d6a140
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-08 09:35:03 +0200

    Fetch and update memes data

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,27 +1,28 @@
 export default function Meme() {
     return (
-    <main>
-        <form className="form">
+        <main>
+            <form className="form">
 
-            <input 
-                type="text" 
-                className="form--input" 
-                placeholder="Shut Up"
-            />
-            
-            <input 
-                type="text" 
-                className="form--input" 
-                placeholder="and take my money"
-            />
+                <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="Shut Up"
+                />
+                
+                <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="and take my money"
+                />
 
-            <button 
-            className="form--button"
-            >
-                Generate a new image 🖼️
-            </button>
-            
-        </form>
-    </main>
+                <button 
+                className="form--button"
+
+                >
+                    Generate a new image 🖼️
+                </button>
+
+            </form>
+        </main>
     );
 }
