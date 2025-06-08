afef0587fda3a105c56b3029741306f91f366841
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-06-08 11:03:40 +0200

    Display meme image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,19 @@
+import React from "react";
+import memesData from "../memesData";
+
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState("")
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
 
                 <input 
                     type="text" 
@@ -17,12 +29,15 @@ export default function Meme() {
 
                 <button 
                 className="form--button"
-
+                onClick={getMemeImage}
                 >
                     Generate a new image 🖼️
                 </button>
 
-            </form>
+            </div>
+
+            <img className="meme--image" src={memeImage} alt="Generated meme" />
+
         </main>
     );
 }
